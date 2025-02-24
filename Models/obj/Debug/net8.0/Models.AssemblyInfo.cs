735d204f5d88cf7ffd46e16402687c4e85d80d5d
ThinhTG <thinhtgse173566@fpt.edu.vn>
2025-02-24 13:11:57 +0700

    commit local NguyenV2

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758f65c4b71c864b2a09555fa049f0414748bc74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be8df93260303cacda069a89934fa1f1297ad9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
