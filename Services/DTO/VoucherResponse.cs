be86de71c3a4bb72d9455f8a08ae30fa0b6fe8d1
Nguyễn Bảo <baolongkiem8@gmail.com>
2025-03-19 13:35:45 +0700

    update vocher get active
    abubu

---
+++ Services/DTO/VoucherResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTO
+{
+    public class VoucherResponse
+    {
+        public Guid VoucherId { get; set; }
+        public string Description { get; set; }
+        public decimal DiscountMoney { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
